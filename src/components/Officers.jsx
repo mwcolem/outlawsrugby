5ba77c35fd558387ff0c47a89c7b92cdaf0f1607
mwcolem <mwcolem@gmail.com>
2018-03-12 19:34:16 -0400

    Start of officer list

--- src/components/Officers.jsx
+++ src/components/Officers.jsx
@@ -2,8 +2,20 @@ import React, {Component} from 'react'
 
 class Officers extends Component {
     render() {
+        var officers = [
+            {name: "Test", email: "email", title: "title", phone: "phone"},
+            {name: "Test2", email: "email2", title: "title2", phone: "phone2"}
+        ]
+
         return (
-            <div>Officers</div>
+            <div>
+                <div>Officers</div>
+                <ul>
+                    {officers.map(function(officer, i) {
+                        return <li key={i}>{officer.name}</li>
+                    })}
+                </ul>
+            </div>
         );
     }
 }
